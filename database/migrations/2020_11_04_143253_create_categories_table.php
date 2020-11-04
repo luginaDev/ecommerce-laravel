79ccf052b23ac21a547d6fd4fe76db3471e0f450
luginaDev <luginaDev@gmail.com>
2020-11-04 22:16:36 +0700

    Database Migrate

---
+++ database/migrations/2020_11_04_143253_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            /*
+                so this category have a child category.
+                if parent_id is null mean that category is parent category 
+                otherwise if the parent_id is not null mean that category is child category
+            */
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
